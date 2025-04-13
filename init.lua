5c63b126ba51128f082f63599e4829afe0b75af1
Juwon <jwlee@emtomega.com>
2025-04-13 21:51:35 +0900

    add cursor in watcher

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ function applicationWatcher(appName, eventType, appObject)
     if (eventType == hs.application.watcher.activated) then
         -- iTerm2 = (appName == 'iTerm2')
         MSoffice = (appName == 'Microsoft Word' or appName == 'Microsoft Excel' or appName == 'Microsoft PowerPoint')
-        vscode = (appName == 'Code')
+        vscode = (appName == 'Code' or 'Cursor')
     end
 end
 appWatcher = hs.application.watcher.new(applicationWatcher)
